be9e57fdf55d6b24f31d758d44fd75d9db4dc54c
Rodgers Munene <munenerodgers72@gmail.com>
2023-09-26 18:06:33 +0300

    modified

--- Grade.java
+++ Grade.java
@@ -1,13 +1,34 @@
+import java.util.Scanner;
+
 public class Grade{
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the unit code for Database: ");
+        String dbms = input.next();
+        System.out.print("Enter the marks for Database: ");
+        int dbmsMarks = input.nextInt();
+        System.out.print("Enter the unit code for Systems: ");
+        String system = input.next();
+        System.out.print("Enter the marks for Systems: ");
+        int systemMarks = input.nextInt();
+        System.out.print("Enter the unit code for Algorithm: ");
+        String algorithm = input.next();
+        System.out.print("Enter the marks for Algorithm: ");
+        int algoMarks = input.nextInt();
+        System.out.print("Enter the unit code for Statistics: ");
+        String statistics = input.next();
+        System.out.print("Enter the marks for Statistics: ");
+        int statisticsMarks = input.nextInt();
+
         String name = "Rodgers Munene";
         String rollNo = "C026-01-0939/2022";
         int currentSemester = 2;
-        int database = 70;
-        int systems = 68;
-        int algorithm = 74;
-        int statistics = 75;
-        int total = database + systems + algorithm + statistics;
+        // int database = 70;
+        // int systems = 68;
+        // int algorithm = 74;
+        // int statistics = 75;
+        int total = dbmsMarks + systemMarks + algoMarks + statisticsMarks;
         float average = total/4f;
         String grade;
         if(average >= 70){
@@ -30,13 +51,13 @@ public class Grade{
         System.out.println("________________________________________________________________");
         System.out.println("|Unit Code                |Unit Name:                     Score:");
         System.out.println("----------------------------------------------------------------");
-        System.out.println("|CCS2101                  |Database                       |"+ database);
+        System.out.println(dbms + "                  |Database                       |"+ dbmsMarks);
         System.out.println("\n");
-        System.out.println("|CCS2102                  |Systems                        |"+ systems);
+        System.out.println(system + "                  |Systems                        |"+ systemMarks);
         System.out.println("\n");
-        System.out.println("|CCS2103                  |Algorithm                      |"+algorithm);
+        System.out.println(algorithm + "                  |Algorithm                      |"+algoMarks);
         System.out.println("\n");
-        System.out.println("|CCS2104                  |Statistics                     |"+ statistics);
+        System.out.println(statistics + "                  |Statistics                     |"+ statisticsMarks);
         System.out.println("----------------------------------------------------------------");
         System.out.println("|Total                                                    |"+ total);
         System.out.println("----------------------------------------------------------------");
